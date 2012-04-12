03124c091730a643544cb70af5058f07beb0a3f3
Ezra Velazquez <ezra.ev@gmail.com>
2012-04-12 02:04:16 -0400

    add analytics tracking

--- index.php
+++ index.php
@@ -29,6 +29,18 @@
 		<script type="text/javascript" src="js/lib/jquery-ui-1.8.18.custom.min.js"></script>
 		<script type="text/javascript" src="js/event_listener.js"></script>
 		<script type="text/javascript" src="js/lib/jquery-ui-timepicker-addon.js"></script>
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-30794173-1']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
 	</head>
 	<body>
 		<?php printFacebokHTML(); ?>
